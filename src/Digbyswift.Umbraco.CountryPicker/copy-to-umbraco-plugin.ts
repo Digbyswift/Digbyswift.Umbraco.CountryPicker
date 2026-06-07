6bbaaec750d107766d771e02d43a4939d781704a
Kieron McIntyre <kieron.mcintyre@digbyswift.com>
2026-06-07 20:48:24 +0100

    Update config

---
+++ src/Digbyswift.Umbraco.CountryPicker/copy-to-umbraco-plugin.ts
@@ -0,0 +1,17 @@
+﻿import * as fs from 'node:fs';
+
+export function copyToUmbracoPlugin() {
+    return {
+        name: 'copy-to-umbraco',
+        closeBundle() {
+            fs.cpSync(
+                'App_Plugins',
+                '../Umbraco.Cms.v17.x/App_Plugins',
+                {
+                    recursive: true,
+                    filter: source => !source.endsWith('.ts')
+                }
+            );
+        }
+    };
+}
